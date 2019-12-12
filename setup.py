c9415cf8954429f29e2ac6401896d9d093a21070
alec-tschantz <tschantz.alec@gmail.com>
2019-12-12 19:54:55 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="planet",
+    version="1.0.0",
+    description=(
+        "Deep Planning Network: Control from pixels by latent planning "
+        + "with learned dynamics."
+    ),
+    license="Apache 2.0",
+    url="https://github.com/alec-tschantz/planet",
+    packages=["planet"],
+)
